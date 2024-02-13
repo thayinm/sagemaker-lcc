62ddf22f67b4f938ca65d18d10c032a6fc9fe552
thayinm <thayinm@amazon.com>
2024-02-13 14:48:56 +0200

    Updated wget

--- jupyter-lab/install-autoshutdown-script/on-start.sh
+++ jupyter-lab/install-autoshutdown-script/on-start.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 set -ex
 # OVERVIEW
-# This script stops a SageMaker notebook once it's idle for more than 1 hour (default time)
+# This script stops a SageMaker Studio Space once it's idle for more than 1 hour (default time)
 # You can change the idle time for stop using the environment variable below.
-# If you want the notebook the stop only if no browsers are open, remove the --ignore-connections flag
 #
-# Note that this script will fail if either condition is not met
-#   1. Ensure the Notebook Instance has internet connectivity to fetch the example config
-#   2. Ensure the Notebook Instance execution role permissions to SageMaker:StopNotebookInstance to stop the notebook 
-#       and SageMaker:DescribeNotebookInstance to describe the notebook.
+# Note that this script will fail if the following condition is not met
+#   1. Ensure the Notebook Instance execution role permissions to SageMaker:DeleteApp to stop the Space
 #
-# PARAMETERS 
-IDLE_TIME=60  ### in seconds. Change this 
+# PARAMETERS
+IDLE_TIME=3600  ### in seconds. Change this
 echo "Fetching the autostop script"
-aws s3 cp <INSERT_S3_URI_HERE> autostop.py
+wget https://raw.githubusercontent.com/thayinm/sagemaker-lcc/main/jupyter-lab/install-autoshutdown-script/autostop.py
+# aws s3 cp <INSERT_S3_URI_HERE> autostop.py
 
 
 echo "Detecting Python install with boto3 install"
 sudo apt-get update -y
-sudo apt-get install -y vim 
+sudo apt-get install -y vim
 sudo sh -c 'printf "#!/bin/sh\nexit 0" > /usr/sbin/policy-rc.d'
 ## required as part of https://stackoverflow.com/questions/46247032/how-to-solve-invoke-rc-d-policy-rc-d-denied-execution-of-start-when-building
 sudo apt-get install -y cron
@@ -38,4 +36,4 @@ fi
 echo "Found boto3 at $PYTHON_DIR"
 
 echo "Starting the SageMaker autostop script in cron"
-echo "*/5 * * * * export AWS_CONTAINER_CREDENTIALS_RELATIVE_URI=$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI; $PYTHON_DIR $PWD/autostop.py --time $IDLE_TIME --region $AWS_DEFAULT_REGION --ignore-connections >> /home/sagemaker-user/autoshutdown.log" | crontab 
+echo "*/5 * * * * export AWS_CONTAINER_CREDENTIALS_RELATIVE_URI=$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI; $PYTHON_DIR $PWD/autostop.py --time $IDLE_TIME --region $AWS_DEFAULT_REGION --ignore-connections >> /home/sagemaker-user/autoshutdown.log" | crontab
